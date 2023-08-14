c71d4de9f864a803edc8484dc476e2da81f2bfbd
Dolan Klock <dolank16@gmail.com>
2023-08-14 19:36:14 -0400

    added api requests

--- lookup/views.py
+++ lookup/views.py
@@ -4,7 +4,14 @@ from django.shortcuts import render
 # Create your views here.
 
 def home(request):
-    return render(request, 'home.html', {})
+    # https://www.airnowapi.org/aq/observation/zipCode/current/?format=application/json&zipCode=89129&distance=25&
+    # API_KEY=66ED8617-4D66-4168-BAB7-371422904ACB
+    import json
+    import requests
+    response = requests.get("https://www.airnowapi.org/aq/observation/zipCode/current/?format=application/json&zipCode=89129&distance=25&API_KEY=66ED8617-4D66-4168-BAB7-371422904ACB")
+    response.raise_for_status()
+    json_data = response.json()
+    return render(request, 'home.html', {"json_data": json_data})
 
 def about(request):
     return render(request, 'about.html', {})
