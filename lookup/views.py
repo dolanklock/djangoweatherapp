253f4120a1585936640e8fe3790fbcbae329281a
Dolan Klock <dolank16@gmail.com>
2023-08-16 12:17:04 -0400

    added more functionality

--- lookup/views.py
+++ lookup/views.py
@@ -14,7 +14,13 @@ def home(request):
         json_data =  None
         return render(request, 'home.html', {"error": error})
     else:
-        return render(request, 'home.html', {"json_data": json_data})
-
+        if json_data[0]["Category"]["Name"] == "Good":
+            category_description = "Enjoy your time outside!"
+        if json_data[0]["Category"]["Name"] == "Moderate":
+            category_description = "If you are unusually sensitive to ozone, consider reducing your activity level or shorten the amount of time you are active outdoors."
+        if json_data[0]["Category"]["Name"] == "Unhealthy for Sensitive Groups":
+            category_description = "Dont go outside"
+        return render(request, 'home.html', {"json_data": json_data, "category_description": category_description})
+    
 def about(request):
     return render(request, 'about.html', {})
