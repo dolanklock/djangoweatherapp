8ae291c8179c456f85bcfdbd99d2c12f748a879d
Dolan Klock <dolank16@gmail.com>
2023-08-16 20:30:48 -0400

    major update

---
+++ lookup/constants.py
@@ -0,0 +1,17 @@
+AIR_QUALITY_IDENTIFIERS = [
+    "good",
+    "moderate",
+    "usg",
+]
+
+AIR_QUALITY_DESCRIPTIONS = {
+        AIR_QUALITY_IDENTIFIERS[0]: "Enjoy your time outside!",
+        AIR_QUALITY_IDENTIFIERS[1]: "If you are unusually sensitive to ozone, consider reducing your activity level or shorten the amount of time you are active outdoors.",
+        AIR_QUALITY_IDENTIFIERS[2]: "Dont go outside",
+}
+
+# AIR_QUALITY_COLORS = {
+#         AIR_QUALITY_IDENTIFIERS[0]: "Enjoy your time outside!",
+#         AIR_QUALITY_IDENTIFIERS[1]: "If you are unusually sensitive to ozone, consider reducing your activity level or shorten the amount of time you are active outdoors.",
+#         AIR_QUALITY_IDENTIFIERS[2]: ,
+# }
--- lookup/views.py
+++ lookup/views.py
@@ -1,13 +1,11 @@
 from django.shortcuts import render
+from . import constants
 
-def home(request):
-    # https://www.airnowapi.org/aq/observation/zipCode/current/?format=application/json&zipCode=89129&distance=25&
-    # API_KEY=66ED8617-4D66-4168-BAB7-371422904ACB
+def main_render(request, zipcode: str):
     import json
     import requests
-
     try:
-        response = requests.get("https://www.airnowapi.org/aq/observation/zipCode/current/?format=application/json&zipCode=89129&distance=25&API_KEY=66ED8617-4D66-4168-BAB7-371422904ACB")
+        response = requests.get(f"https://www.airnowapi.org/aq/observation/zipCode/current/?format=application/json&zipCode={zipcode}&distance=25&API_KEY=66ED8617-4D66-4168-BAB7-371422904ACB")
         json_data = response.json()
     except Exception as ex:
         error = f"{ex}"
@@ -15,12 +13,64 @@ def home(request):
         return render(request, 'home.html', {"error": error})
     else:
         if json_data[0]["Category"]["Name"] == "Good":
-            category_description = "Enjoy your time outside!"
+            category_description = constants.AIR_QUALITY_DESCRIPTIONS["good"]
+            category_color = "good"
         if json_data[0]["Category"]["Name"] == "Moderate":
-            category_description = "If you are unusually sensitive to ozone, consider reducing your activity level or shorten the amount of time you are active outdoors."
+            category_description = constants.AIR_QUALITY_DESCRIPTIONS["moderate"]
+            category_color = "moderate"
         if json_data[0]["Category"]["Name"] == "Unhealthy for Sensitive Groups":
-            category_description = "Dont go outside"
-        return render(request, 'home.html', {"json_data": json_data, "category_description": category_description})
+            category_description = constants.AIR_QUALITY_DESCRIPTIONS["usg"]
+            category_color = "usg"
+        return render(request, 'home.html', {"json_data": json_data,
+                                            "category_description": category_description,
+                                            "category_color": category_color,
+                                            })
+
+def home(request):
+    if request.method == "POST":  # checking if request is a POST request
+        zipcode = request.POST["zipcode"]  # getting the value from 'name' attirbute from base html
+        return main_render(request=request, zipcode=zipcode)  # has to return the HTTPS response
     
+    else:
+        return main_render(request=request, zipcode="89129")  # has to return the HTTPS response
+
+
 def about(request):
     return render(request, 'about.html', {})
+
+
+
+
+# def home(request):
+#     import json
+#     import requests
+
+#     if request.method == "POST":  # checking if request is a POST request
+#         zipcode = request.POST["zipcode"]  # getting the value from 'name' attirbute from base html
+#         try:
+#             response = requests.get(f"https://www.airnowapi.org/aq/observation/zipCode/current/?format=application/json&zipCode={zipcode}&distance=25&API_KEY=66ED8617-4D66-4168-BAB7-371422904ACB")
+#             json_data = response.json()
+#         return render(request, 'home.html', {'zipcode': zipcode})
+    
+#     else:
+#         try:
+#             response = requests.get("https://www.airnowapi.org/aq/observation/zipCode/current/?format=application/json&zipCode=89129&distance=25&API_KEY=66ED8617-4D66-4168-BAB7-371422904ACB")
+#             json_data = response.json()
+#         except Exception as ex:
+#             error = f"{ex}"
+#             json_data =  None
+#             return render(request, 'home.html', {"error": error})
+#         else:
+#             if json_data[0]["Category"]["Name"] == "Good":
+#                 category_description = constants.AIR_QUALITY_DESCRIPTIONS["good"]
+#                 category_color = "good"
+#             if json_data[0]["Category"]["Name"] == "Moderate":
+#                 category_description = constants.AIR_QUALITY_DESCRIPTIONS["moderate"]
+#                 category_color = "moderate"
+#             if json_data[0]["Category"]["Name"] == "Unhealthy for Sensitive Groups":
+#                 category_description = constants.AIR_QUALITY_DESCRIPTIONS["usg"]
+#                 category_color = "usg"
+#             return render(request, 'home.html', {"json_data": json_data,
+#                                                 "category_description": category_description,
+#                                                 "category_color": category_color,
+#                                                 })
